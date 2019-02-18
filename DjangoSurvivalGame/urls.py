18ab779e0f106656ec5561386b291465635af142
Otto Hiltunen <2268682h@student.gla.ac.uk>
2019-02-18 15:08:22 +0000

    created dirs for our app templates as well as registration templates

--- DjangoSurvivalGame/urls.py
+++ DjangoSurvivalGame/urls.py
@@ -18,16 +18,16 @@ from django.conf import settings
 from django.contrib import admin
 from django.conf.urls import include
 from django.conf.urls.static import static
-from rango import views
+from Game import views
 from registration.backends.simple.views import RegistrationView
 
 class MyRegistrationView(RegistrationView):
     def get_success_url(self, user):
-        return '/rango/'
+        return '/Game/'
 
 app_name = 'Game'
 urlpatterns = [
-    url(r'^$', views.index, name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^Game/',include('Game.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name = 'registration_register'),
