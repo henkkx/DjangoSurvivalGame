6bb84049dbf88c5378a332bb0784eb19f70577c2
Matthew Walker <2310528w@student.gla.ac.uk>
2019-02-20 18:09:16 +0000

    updated admin.py: Model entries now have orderings

--- Game/admin.py
+++ Game/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 from Game.models import Player, Badge, Achievement
 
-#These classes allow us to add extra functionality to the admin page
+'''
+These classes allow us to add extra functionality to the admin page.
+-list_display allows specified fields to be displayed on admin interface for that model.
+-ordering determines which field ordering of database entries is based on for that model.
+
+'''
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('user', 'most_dayes_survived', 'most_kills', 'most_people')
+    list_display = ('user', 'most_dayes_survived', 'most_kills', 'most_people', 'most_exp')
+    ordering = ['-most_exp']
     
 class AchievementAdmin(admin.ModelAdmin):
     list_display = ('player', 'badge', 'date_awarded')
 
 class BadgeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'criteria', 'badge_type', 'icon')
+    ordering = ['badge_type', 'criteria']
 
 # Register your models here.
 admin.site.register(Player, PlayerAdmin)
