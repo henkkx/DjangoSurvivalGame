4e8caef848b1352c85546137792d00a19a9dc34f
Matthew Walker <2310528w@student.gla.ac.uk>
2019-02-20 18:22:07 +0000

    updated admin.py: Can now reset games played

--- Game/admin.py
+++ Game/admin.py
@@ -5,11 +5,18 @@ from Game.models import Player, Badge, Achievement
 These classes allow us to add extra functionality to the admin page.
 -list_display allows specified fields to be displayed on admin interface for that model.
 -ordering determines which field ordering of database entries is based on for that model.
-
+-actions can be applied to all selected model entries. By default the only availiable action is
+delete selected.
 '''
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('user', 'most_dayes_survived', 'most_kills', 'most_people', 'most_exp')
+    list_display = ('user', 'most_dayes_survived', 'games_played', 'most_kills', 'most_people', 'most_exp')
     ordering = ['-most_exp']
+    actions = ['reset_games_played']
+
+    def reset_games_played(self, request, queryset):
+        queryset.update(games_played=0)
+    reset_games_played.short_description = "Reset games played for selected players"
+
     
 class AchievementAdmin(admin.ModelAdmin):
     list_display = ('player', 'badge', 'date_awarded')
