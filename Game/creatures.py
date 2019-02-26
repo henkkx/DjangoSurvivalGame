b782e115e4b50df66e7e5aac165185dbcb10a030
Matthew Walker <2310528w@student.gla.ac.uk>
2019-02-26 12:31:29 +0000

    Made Creature class abstract

--- Game/creatures.py
+++ Game/creatures.py
@@ -1,18 +1,18 @@
-class Creature:
+from abc import ABC, abstractmethod
+class Creature(ABC):
 
-    def __init__(self, name, type, hp, ap, vulnerability, immune, pl):
+    def __init__(self, name, hp, ap, vulnerability, immune, pl):
 
         self.name = name
-        self.type = type
         self.hp = hp
         self.ap = ap
         self.vulnerability = vulnerability
         self.immune = immune
         self.power_level = pl
+        super().__init__()
 
+    @abstarctmethod
     def __str__(self):
+        pass
+        
 
-        return "{0} ({1}, Health Points : {2}, Difficulty : {3} )".format(self.name, self.type, self.hp
-                                                                          ,self.power_level)
-        return "{0} ({1}, Health Points : {2}, Difficulty : {3} )".format(self.name, self.type, self.hp, self.power_level)
-        #       BoatyMcBoatface (Spider, Health Points : 1000, Difficulty : Hard)
