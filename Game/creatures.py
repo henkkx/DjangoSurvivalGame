dc989a08c3f2e9c313974b48970f91fe70943aec
Matthew Walker <2310528w@student.gla.ac.uk>
2019-02-26 12:42:12 +0000

    Added Spider subclass to creatures.py and fixed some syntax

--- Game/creatures.py
+++ Game/creatures.py
@@ -11,8 +11,26 @@ class Creature(ABC):
         self.power_level = pl
         super().__init__()
 
-    @abstarctmethod
+    @abstractmethod
     def __str__(self):
         pass
         
 
+class Spider(Creature):
+
+    def __init__(self, name, pl):
+        hp = 10 * pl
+        ap = 2 * pl
+        super().__init__(name, hp, ap, ["Fire"], ["Water"], pl)
+        
+
+    def __str__(self):
+        return ("{0} (Name: {1}, Health Points: {2},  Attack Points: {3}, Weaknesses: {4}, Strengths: {5}, Level: {6})".format(self.__class__.__name__, self.name, self.hp, self.ap, self.vulnerability, self.immune, self.power_level))
+
+
+#Class Tests
+spider = Spider("Shelob", 5)
+print(spider.__str__())
+
+
+    
