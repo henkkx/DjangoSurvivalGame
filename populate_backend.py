07873ee9e429fffee4b6570f7ea59e09b17031c8
Otto Hiltunen <2268682h@student.gla.ac.uk>
2019-03-05 13:20:08 +0000

    added populate to root

--- populate_backend.py
+++ populate_backend.py
@@ -7,8 +7,6 @@ django.setup()
 from Game.models import Badge
 
 
-
-
 def populate():
     badges = [
         {'name': 'First Blood',
@@ -40,3 +38,6 @@ def populate():
 def add_badge(name, desc, criteria, type_of):
     badge = Badge.objects.get_or_create(name=name, description=desc, criteria=criteria, type=type_of)
     return badge
+
+if __name__ == "__main__":
+    populate()
