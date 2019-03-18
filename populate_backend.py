3d6d0f462af79f428373b498491821feda2e3f8d
Matthew Walker <2310528w@student.gla.ac.uk>
2019-03-18 13:20:18 +0000

    Added players to population script + minor fix

--- populate_backend.py
+++ populate_backend.py
@@ -4,7 +4,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 
 import django
 django.setup()
-from Game.models import Badge
+from Game.models import Badge, Player
+from django.contrib.auth.models import User
 
 badges = [
     {'name': 'First Blood',
@@ -50,15 +51,35 @@ badges = [
 
 ]
 
+players = []
+for i in range(0,10):
+    players.append({"username":"Player{0}".format(i), "email": "{0:06d}{1}@student.gla.ac.uk".format(i, chr(65+i)),
+                "password": "{0}{1}{2}{3}{4}{5}{6}{7}".format(chr(i+65),chr(i+65),chr(i+66),chr(i+67),chr(i+68),chr(i+69),chr(i+70),chr(i+71)),
+                 "games_played": i*10, "most_days_survived": i*5, "most_kills": i*4, "most_people": i*2, "most_exp": 100*i})
+
 def populate():
 
     for badge in badges:
-        add_badge(badge['name'], badge['description'], badge['criteria'], badge['type'])
+        add_badge(badge['name'], badge['description'], badge['criteria'], badge['badge_type'])
+
+    for player in players:
+        add_player(player["username"], player["email"], player["password"], player["games_played"], player["most_days_survived"], player["most_kills"],
+                       player["most_people"], player["most_exp"])
 
 
 def add_badge(name, desc, criteria, type_of):
-    badge = Badge.objects.get_or_create(name=name, description=desc, criteria=criteria, type=type_of)
+    badge = Badge.objects.get_or_create(name=name, description=desc, criteria=criteria, badge_type=type_of)
     return badge
+
+def add_player(username, email, password, games_played, most_days_survived, most_kills,
+               most_people, most_exp, picture = None):
+    new_user = User.objects.create_user(username=username,
+                                                email=email,
+                                               password=password)
+    
+    Player.objects.create(user=new_user, picture=picture, games_played=games_played,
+                          most_days_survived=most_days_survived, most_kills=most_kills,
+                          most_people=most_people, most_exp=most_exp)
 
 if __name__ == "__main__":
     populate()
