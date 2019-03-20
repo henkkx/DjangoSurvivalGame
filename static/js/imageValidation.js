9f9ac728545251fb8094e0f0df78c7dd4845eab1
Matthew Walker <2310528w@student.gla.ac.uk>
2019-03-20 21:22:39 +0000

    404 redirect fix

--- static/js/imageValidation.js
+++ static/js/imageValidation.js
@@ -1,11 +1,11 @@
 function NoImageSelect() {
-  if (document.getElementById("id_picture").files.length === 0) {
+  if (document.getElementById("file-input").files.length == 0) {
     alert("Please Select An Image To Upload");
     return false;
   }
   if (
     !document
-      .getElementById("id_picture")
+      .getElementById("file-input")
       .files[0].name.match(/.(jpg|jpeg|png|gif)$/i)
   ) {
     alert("The file selected is not of an Image type");
