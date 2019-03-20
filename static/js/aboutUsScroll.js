94e3a42c0f1313f7b3b5c752751237c9fd304e92
Otto Hiltunen <2268682h@student.gla.ac.uk>
2019-03-20 16:13:00 +0000

    added scroll efects to home, instructions pages

--- static/js/aboutUsScroll.js
+++ static/js/aboutUsScroll.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
   
         $('html, body').animate({
           scrollTop: $(hash).offset().top
-        }, 1000, function(){
+        }, 1200, function(){
      
           // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
